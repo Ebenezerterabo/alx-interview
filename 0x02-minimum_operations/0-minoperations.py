14649e8bb0f32cfe4ca6ce97f9bf01510eb10674
Ebenezerterabo <Ebenezerterabo@gmail.com>
2024-08-27 01:04:53 +0000

    Task 0 modified solution

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """ 0-minoperations """
 
 
 def minOperations(n: int) -> int:
-    """ 
+    """
     returns the fewest number of operations needed
 
     Args:
@@ -11,26 +11,18 @@ def minOperations(n: int) -> int:
 
     Returns:
         number of operations
-
-    - base case
-    - variable to keep track of total number of operations
-    - variable to keep track of current factor
-
-    - divide n by factor until n is divisible by factor
-    - add factor to operations
-    - divide n by factor
-
-    - return the total number of operations
     """
-    if n <= 1:
+    if n <= 1:  # base case
         return 0
 
-    operations = 0
-    factor = 2
+    operations = 0  # variable to keep track of total number of operations
+    factor = 2  # variable to keep track of current factor
 
     while n > 1:
+        # divide n by factor until n is divisible by factor
         while n % factor == 0:
-            operations += factor
-            n //= factor
-        factor += 1
-    return operations
+            operations += factor  # add factor to operations
+            n //= factor  # divide n by factor
+        factor += 1  # move to the next factor
+
+    return operations  # return the total number of operations
